52ac49bd9c35e07694b9ad578621d8d126eb637a
SG <sven.ginka@metagons-software.de>
2025-08-11 18:39:26 +0200

    parameter read out

--- read_kwp1281_id.py
+++ read_kwp1281_id.py
@@ -4,9 +4,14 @@ import queue
 import serial
 import threading
 
-ADDRESS = 0x1  # Engine ECU
+#ADDRESS = 0x1  # Engine ECU
+ADDRESS = 0x17  # instruments
 BIT_DURATION = 0.20  # 200 ms = 5 baud
-SOURCE = 0xF1
+
+BLOCK_END = 0x03
+
+CMD_ACK = 0x09
+CMD_READ_GROUP = 0x29
 
 class OBD:
     def __init__(self):
@@ -15,8 +20,8 @@ class OBD:
         else:
             self.ser = serial_for_url('ftdi:///1', baudrate=10400, timeout=0.01)
         self.ser.flush()
-        self.debug = True
-        self.init_complete = False
+        self.low_level_debug = False
+        self.block_counter = 0
         self.q = queue.Queue()
 
         self.rx_thread = threading.Thread(target=self.read_loop, args=())
@@ -54,7 +59,7 @@ class OBD:
             if len(a) > 0:
                 for c in a:
                     self.q.put(c)
-                    if debug:
+                    if self.low_level_debug:
                         print(f"> 0x{c:02X}")
             else:
                 time.sleep(0.01)
@@ -63,7 +68,7 @@ class OBD:
         while True:
             try:
                 first = self.q.get(timeout=1)
-                if self.debug:
+                if self.low_level_debug:
                     print(f"rx> 0x{first:02x}")
                 return first
             except queue.Empty:
@@ -73,7 +78,10 @@ class OBD:
             else:
                 break
         return None
+
     def write_byte(self, data_byte):
+        if self.low_level_debug:
+            print(f"tx> 0x{data_byte:02X}")
         self.ser.write(bytes([data_byte]))
 
     def wait_byte(self, byte, timeout=1):
@@ -88,30 +96,6 @@ class OBD:
             if t > timeout:
                 return False
 
-    def send_bytes(self, data_bytes):
-        for b in data_bytes:
-            self.ser.write(bytes([b]))
-            # echo
-            e = self.wait_byte(b, timeout=1)
-            if not e:
-                return False
-        return True
-
-    def send_acknowledge(self, byte):
-        # answer = 0xff - byte
-        answer = (~byte) & 0xFF
-        if self.debug:
-            print(f"ackn> 0x{answer:02x}")
-        return self.send_bytes(bytes([answer]))
-
-    def read_sequence(self):
-        self.wait_byte(0x55)
-        key1 = self.read_byte()
-        key2 = self.read_byte()
-        v = self.read_byte()
-        print(f"value {key1:02X} {key2:02X} {v}")
-        self.send_acknowledge(v)
-
     def print_hex_bytes(self, bytes, prefix=""):
         print(f"{prefix}>", end="")
         for byte in bytes:
@@ -124,30 +108,101 @@ class OBD:
             print(f"{chr(byte)}", end="")
         print("")
 
-    def cs_xor(self, data):
-        c = 0
+    def read_ecu_block(self):
+
+        buffer = bytearray()
+
+        block_length = self.read_byte()
+        answer = (~block_length) & 0xFF
+        self.write_byte(answer)
+        echo = self.read_byte()
+        if echo != answer:
+            return None
+
+        print(f"reading response len {block_length}")
+        for i in range(1, block_length):
+            d = self.read_byte()
+
+            # decide what to do
+            if i == 1:
+                self.block_counter = d
+            elif i == 2:
+                block_type = d
+
+            buffer.append(d)
+
+            # send ackn
+            answer = (~d) & 0xFF
+            self.write_byte(answer)
+            echo = self.read_byte()
+            if echo != answer:
+                return None
+
+        # last byte should be BLOCK_END
+        b = self.read_byte()
+        if b != BLOCK_END:
+            return None
+
+        self.print_hex_bytes(buffer, "hex")
+        self.print_ascii_bytes(buffer, "ascii")
+        return buffer
+
+    def write_raw_block(self, block):
+        self.print_hex_bytes(block, "write CMD")
+        for b in block:
+            self.write_byte(b)
+            echo = self.read_byte()
+            if b != echo:
+                return False
+            answer = self.read_byte()
+            expected_answer = (~b) & 0xFF
+            if answer != expected_answer:
+                return False
+        # EOF
+        self.write_byte(BLOCK_END)
+        echo = self.read_byte()
+        if echo != BLOCK_END:
+            return False
+        return True
+
+    def write_ecu_block(self, cmd, data=None):
+
+        block = []
+        if data is None:
+            data = []
+
+        length = 3 + len(data)
+
+        # length
+        block.append(length)
+
+        # block counter
+        self.block_counter += 1
+        if self.block_counter > 255:
+            self.block_counter = 0
+
+        block.append(self.block_counter)
+
+        # cmd
+        block.append(cmd)
+
+        # optional data
         for b in data:
-            c ^= b
-        return c & 0xFF
+            block.append(b)
 
-    def send_id_request(self):
-
-        for block_count in range(2,20):
-            b = bytearray([0x3, block_count, 0x09, 0x03])
-            for i in b:
-                self.write_byte(i)
-                echo = self.read_byte()
-
-
+        # EOF block
+        return self.write_raw_block(block)
 
 
     def initialize_ecu(self):
         complete = False
 
         while not complete:
+
+            # weak up ecu
             self.send_5baud_7O1(ADDRESS)
 
-        # read start byte
+            # read start byte
             self.wait_byte(0x55, timeout=10)
 
             while True:
@@ -155,39 +210,48 @@ class OBD:
                 key2 = self.read_byte(wait=False)
                 print(f"key2 {key2:02x}")
 
-                #time.sleep(0.1)
-                answer = (~key2) & 0xFF
                 answer = 0xff - key2
                 print(f"answer 0x{answer:02X}")
                 self.ser.write(bytes([answer]))
 
                 v = self.read_byte(wait=False)
                 if v == answer:
+                    # echo from ecu
                     complete = True
                     break
                 elif v == 0x55:
+                    # restarted communication
                     continue
                 else:
                     # timeout
                     break
 
-        self.init_complete = True
+        # after connection, we start reading blocks
+        res = self.read_ecu_block()
+        if res is None:
+            return False
+        else:
+            return True
 
-        BLOCK_END = 0x03
-        buf = []
+    def endless_block_reading_loop(self):
         while True:
-            ecu = self.read_byte()
-            buf.append(ecu)
-            if ecu == BLOCK_END:
+            for i in range(1, 256):
+                print(f"reading group {i}")
+                if not self.write_ecu_block(CMD_READ_GROUP, data=[i]):
+                    print(f"Failed to write ECU block {CMD_READ_GROUP}")
+                    break
+                if self.read_ecu_block() is None:
+                    print(f"Failed to read ECU block {CMD_READ_GROUP}")
+                    break
+
+            if not self.write_ecu_block(CMD_ACK):
+                print("Failed to write ECU block")
                 break
-            answer = (~ecu) & 0xFF
-            self.write_byte(answer)
-            echo = self.read_byte()
-            # discard echo
+            if self.read_ecu_block() is None:
+                print("Failed to read ECU block")
+                break
 
-        self.print_hex_bytes(buf, "hex")
-        self.print_ascii_bytes(buf, "ascii")
-
+        return
 
 
 def main():
@@ -196,12 +260,13 @@ def main():
         obd = OBD()
 
 
-        obd.initialize_ecu()
+        if not obd.initialize_ecu():
+            print("init failed")
+            return
+
         print("init complete")
 
-        obd.send_id_request()
-
-
+        obd.endless_block_reading_loop()
 
         # rx_thread.join(timeout=2)
         # ser.close()
